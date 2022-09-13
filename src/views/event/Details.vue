11760ed3c825dd58e9a843e2fc0148b33acf9da9
Otavio Guido Silva <otavio.guido-ext@ab-inbev.com>
2022-09-13 15:36:17 -0300

    finished lesson 4

---
+++ src/views/event/Details.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1>{{ event.title }}</h1>
+  <p>{{ event.time }} on {{ event.date }} @ {{ event.location }}</p>
+  <p>{{ event.description }}</p>
+</template>
+
+<script>
+export default {
+  props: ['event']
+}
+</script>
